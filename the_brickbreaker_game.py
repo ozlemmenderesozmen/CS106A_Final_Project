7ac7b30d843869ffc4c40e613284d6d5a2e1f1c8
Ozlem Ozmen <ozlemmenderes@gmail.com>
2020-06-04 19:34:54 +0200

    9 more levels added.

--- the_brickbreaker_game.py
+++ the_brickbreaker_game.py
@@ -1,10 +1,10 @@
 """
 Brick Breaker Game
-The player must crush a wall of bricks by deflecting a bouncing ball with a paddle.
+The player must smash a wall of bricks by deflecting a bouncing ball with a paddle.
 The paddle may move horizontally and is controlled with the computer's mouse.
 The player gets 3 lives to start with; a life is lost if the ball hits the bottom of the screen.
-When all the bricks have been destroyed, the player wins the game.
-There is only 1 level in the game.
+When all the bricks have been destroyed in all levels, the player wins the game.
+The game consist of 10 levels.
 """
 
 import tkinter
@@ -12,15 +12,14 @@ import time
 import keyboard
 
 # How big is the playing area?
-CANVAS_WIDTH = 600  # Width of drawing canvas in pixels
-CANVAS_HEIGHT = 800  # Height of drawing canvas in pixels
+CANVAS_WIDTH = 600
+CANVAS_HEIGHT = 800
 
 # Constants for the bricks
-N_ROWS = 8  # How many rows of bricks are there?
-N_COLS = 10  # How many columns of bricks are there?
-SPACING = 5  # How much space is there between each brick?
-BRICK_START_Y = 50  # The y coordinate of the top-most brick
-BRICK_HEIGHT = 20  # How many pixels high is each brick
+N_COLS = 10
+SPACING = 5
+BRICK_START_Y = 100
+BRICK_HEIGHT = 20
 BRICK_WIDTH = (CANVAS_WIDTH - (N_COLS + 1) * SPACING) / N_COLS
 
 # Constants for the ball and paddle
@@ -30,63 +29,137 @@ PADDLE_WIDTH = 120
 
 CHANGE_X = 10
 CHANGE_Y = 10
+AVAILABLE_LEVEL_COUNT = 10
 
 
 def main():
-    count = 0
-    # playing area is created
+    level = 1
     canvas = make_canvas(CANVAS_WIDTH, CANVAS_HEIGHT, 'Brick Breaker')
-    for i in range(3):
-        count += 1
-        # playing elements are created
-        bricks = building_bricks(canvas)
-        ball = create_a_ball(canvas)
-        paddle = create_paddle(canvas)
-        canvas.update()
-        # with pressing space from keyboard, the game starts
-        wait_until_player_press_space()
-        play_brick_breaker_game(canvas, ball, paddle, bricks, count)
-        canvas.update()
-        wait_until_player_press_space()
-        canvas.delete(tkinter.ALL)
+    totally_failed = play(canvas, level)
+    if totally_failed:
+        display_text(canvas, "GAME OVER")
+    else:
+        display_text(canvas, "YOU WIN")
+    canvas.update()
+    canvas.mainloop()
 
 
-def wait_until_player_press_space():
-    # game stops until player press space
-    while not press_space():
-        time.sleep(1 / 10)
+def play(canvas, level):
+    # runs the game until '3 times failing' or 'completing all levels'
+    fail_count = 0
+    while fail_count < 3 and level <= AVAILABLE_LEVEL_COUNT:
+        display_left_life(canvas, fail_count)
+        display_level_number(canvas, level)
+        failed = play_a_level(canvas, level)
+        if failed:
+            fail_count += 1
+        else:
+            level += 1
+
+    return fail_count == 3
 
 
-def building_bricks(canvas):
+def display_left_life(canvas, fail_count):
+    # displays the lives left at top-right of the canvas
+    left_life_count = 3 - fail_count
+    canvas.create_text(500, 50, fill="black", font="ComicSans 20 bold",
+                       text="Life: " + str(left_life_count))
+
+
+def display_level_number(canvas, level):
+    # displays the level number at top-left of the canvas
+    canvas.create_text(100, 50, fill="black", font="ComicSans 20 bold",
+                       text="Level " + str(level))
+
+
+def tell_player_to_press_space_and_wait(canvas):
+    # starts the game
+    display_press_space_text(canvas)
+    wait_until_player_press_space(canvas)
+
+
+def play_a_level(canvas, level):
+    # creates game elements and starts and ends a level of the game
+    bricks = build_bricks(canvas, level, N_COLS, level)
+    ball = create_a_ball(canvas)
+    paddle = create_paddle(canvas)
+
+    tell_player_to_press_space_and_wait(canvas)
+    failed = move_ball_interactively(canvas, ball, paddle, bricks, level)
+    tell_player_to_press_space_and_wait(canvas)
+    canvas.delete(tkinter.ALL)
+
+    return failed
+
+
+def build_bricks(canvas, n_rows, n_cols, level):
     # returns and creates bricks
     bricks = []
-    for row in range(N_ROWS):
-        for col in range(N_COLS):
-            a_brick = create_a_brick(canvas, row, col)
+    for row in range(n_rows):
+        for col in range(n_cols):
+            a_brick = create_a_brick(canvas, row, col, level)
             bricks.append(a_brick)
 
     return bricks
 
 
-def create_a_brick(canvas, row, col):
+def create_a_brick(canvas, row, col, level):
     # creates and returns a brick
     left_x = col * (SPACING + BRICK_WIDTH)
     top_y = BRICK_START_Y + row * (BRICK_HEIGHT + SPACING)
     end_x = left_x + (CANVAS_WIDTH - (N_COLS + 1) * SPACING) / N_COLS
     end_y = top_y + BRICK_HEIGHT
-    colour = get_colour(row)
+    colour = choose_brick_colour(row, level)
     brick = canvas.create_rectangle(left_x, top_y, end_x, end_y, fill=colour, outline='white')
 
     return brick
 
 
-def get_colour(row):
-    # returns the colour for bricks
-    if row == 0 or row == 1 or row == 2:
-        return 'red'
-    if row == 3 or row == 4 or row == 5:
+def choose_brick_colour(row, level):
+    # returns the colour for bricks according to level and row
+    if level == 1 and row == 0:
+        return 'blue'
+    elif level == 2 and (row == 0 or row == 1):
+        return 'blue'
+    elif level == 3 and (row == 0 or row == 1 or row == 2):
+        return 'blue'
+    elif level == 4 and row == 0:
         return 'yellow'
-    else:
+    elif level == 4 and (row == 1 or row == 2 or row == 3):
+        return 'blue'
+    elif level == 5 and (row == 0 or row == 1):
+        return 'yellow'
+    elif level == 5 and (row == 2 or row == 3 or row == 4):
+        return 'blue'
+    elif level == 6 and (row == 0 or row == 1 or row == 2):
+        return 'yellow'
+    elif level == 6 and (row == 3 or row == 4 or row == 5):
+        return 'blue'
+    elif level == 7 and row == 0:
+        return 'purple'
+    elif level == 7 and (row == 1 or row == 2 or row == 3):
+        return 'yellow'
+    elif level == 7 and (row == 4 or row == 5 or row == 6):
+        return 'blue'
+    elif level == 8 and (row == 0 or row == 1):
+        return 'purple'
+    elif level == 8 and (row == 2 or row == 3 or row == 4):
+        return 'yellow'
+    elif level == 8 and (row == 5 or row == 6 or row == 7):
+        return 'blue'
+    elif level == 9 and (row == 0 or row == 1 or row == 2):
+        return 'purple'
+    elif level == 9 and (row == 3 or row == 4 or row == 5):
+        return 'yellow'
+    elif level == 9 and (row == 6 or row == 7 or row == 8):
+        return 'blue'
+    elif level == 10 and row == 0:
+        return 'red'
+    elif level == 10 and (row == 1 or row == 2 or row == 3):
+        return 'purple'
+    elif level == 10 and (row == 4 or row == 5 or row == 6):
+        return 'yellow'
+    elif level == 10 and (row == 7 or row == 8 or row == 9):
         return 'blue'
 
 
@@ -104,41 +177,51 @@ def create_a_ball(canvas):
 def create_paddle(canvas):
     # creates and returns the paddle
     paddle = canvas.create_rectangle(0, PADDLE_Y, PADDLE_WIDTH, CANVAS_HEIGHT - 20, fill='black')
+
     return paddle
 
 
-def play_brick_breaker_game(canvas, ball, paddle, bricks, count):
-    # starts the game and designs the conditions of how it ends
-    dx = CHANGE_X
-    dy = CHANGE_Y
-    while True:
-        move_paddle(canvas, paddle)  # move paddle
+def display_press_space_text(canvas):
+    # displays to player the text on how to start the game
+    press_space_text = canvas.create_text(300, 500, fill="black", font="ComicSans 20 bold",
+                                          text="Press 'space' to start")
+    canvas.update()
+    canvas.delete(press_space_text)
 
-        canvas.move(ball, dx, dy)  # move ball
-        if hit_left_wall(canvas, ball) or hit_right_wall(canvas, ball):  # if ball hits left or right wall
+
+def wait_until_player_press_space(canvas):
+    # game stops until player press space
+    while not space_pressed():
+        time.sleep(1 / 10)
+
+
+def move_ball_interactively(canvas, ball, paddle, bricks, level):
+    # changes the direction of ball in case of hitting a wall or paddle and returns fail
+    dx, dy = pace_of_ball(level)
+    failed = False
+
+    while True:
+        move_paddle(canvas, paddle)
+        canvas.move(ball, dx, dy)
+
+        if ball_hits_left_wall(canvas, ball) or ball_hits_right_wall(canvas, ball):
             dx *= -1
-        elif hit_top_wall(canvas, ball):  # if ball hits top wall
+        elif ball_hits_top_wall(canvas, ball):
             dy *= -1
-        elif hit_paddle(canvas, paddle):  # if ball hits paddle
+        elif ball_hits_canvas_object(canvas, paddle):
             dx, dy = bounce_ball_when_hits_paddle(canvas, ball, paddle, dx, dy)
-        elif is_ball_lower_than_paddle(canvas, ball, paddle):  # if ball is in lower level than paddle
-            if count == 1:
-                two_more_play(canvas)
-                break
-            elif count == 2:
-                one_more_play(canvas)
-                break
-            elif count == 3:
-                game_over(canvas)
-                break
-        elif hit_bricks(canvas, bricks):  # if ball hits bricks
+        elif ball_hits_bricks(canvas, bricks):
             dy *= -1
+        elif is_ball_lower_than_paddle(canvas, ball, paddle):
+            failed = True
+            break
         elif len(bricks) == 0:  # if ball hits all bricks and there are no more bricks
-            win_the_game(canvas)
             break
 
         canvas.update()         # redraw canvas
         time.sleep(1 / 50.)       # pause
+
+    return failed
 
 
 def move_paddle(canvas, paddle):
@@ -148,28 +231,25 @@ def move_paddle(canvas, paddle):
     canvas.moveto(paddle, paddle_left_x, PADDLE_Y)
 
 
-def two_more_play(canvas):
-    # first try
-    canvas.create_text(300, 500, fill="black", font="Times 30 bold",
-                       text="2 MORE")
+def pace_of_ball(level):
+    # calculates and returns the speed of ball according to levels
+    if level == 1 or level == 2 or level == 3:
+        dx = CHANGE_X * 0.75
+        dy = CHANGE_Y * 0.75
+    elif level == 4 or level == 5 or level == 6 or level == 7:
+        dx = CHANGE_X
+        dy = CHANGE_Y
+    else:
+        dx = CHANGE_X * 1.25
+        dy = CHANGE_Y * 1.25
+
+    return dx, dy
 
 
-def one_more_play(canvas):
-    # second try
-    canvas.create_text(300, 500, fill="black", font="Times 30 bold",
-                       text="1 MORE")
-
-
-def game_over(canvas):
-    # third try
-    canvas.create_text(300, 500, fill="red", font="Times 30 bold",
-                       text="GAME OVER")
-
-
-def win_the_game(canvas):
-    # writes to the screen that player win the game
-    canvas.create_text(300, 500, fill="black", font="Times 30 bold",
-                       text="YOU WIN")
+def display_text(canvas, message):
+    # display the text in the middle of the canvas such as 'game over', 'you win'
+    canvas.create_text(300, 500, fill="red", font="ComicSans 30 bold",
+                       text=message)
 
 
 def is_ball_lower_than_paddle(canvas, ball, paddle):
@@ -177,17 +257,17 @@ def is_ball_lower_than_paddle(canvas, ball, paddle):
     return get_bottom_y(canvas, ball) > get_top_y(canvas, paddle)
 
 
-def hit_paddle(canvas, paddle):
+def ball_hits_canvas_object(canvas, canvas_object):
     # this graphics method gets the location of the paddle as a list
-    paddle_coords = canvas.coords(paddle)
+    canvas_object_coords = canvas.coords(canvas_object)
 
     # the list has four elements:
-    x1 = paddle_coords[0]
-    y1 = paddle_coords[1]
-    x2 = paddle_coords[2]
-    y2 = paddle_coords[3]
+    x1 = canvas_object_coords[0]
+    y1 = canvas_object_coords[1]
+    x2 = canvas_object_coords[2]
+    y2 = canvas_object_coords[3]
 
-    # we can then get a list of all objects in that area
+    # we get a list of all objects in that area
     results = canvas.find_overlapping(x1, y1, x2, y2)
 
     return len(results) > 1
@@ -199,82 +279,67 @@ def bounce_ball_when_hits_paddle(canvas, ball, paddle, dx, dy):
         dx *= -1
     elif get_left_x(canvas, ball) >= (get_right_x(canvas, paddle) - BALL_SIZE / 2) and dx < 0:
         dx *= -1
+
     dy *= -1
+
     return dx, dy
 
 
-def hit_bricks(canvas, bricks):
+def ball_hits_bricks(canvas, bricks):
+    # changes colour or delete the bricks when the ball hits the bricks
     for brick in bricks:
-        # this graphics method gets the location of the bricks as a list
-        brick_coords = canvas.coords(brick)
+        is_hit_this_brick = ball_hits_canvas_object(canvas, brick)
 
-        # the list has four elements:
-        x1 = brick_coords[0]
-        y1 = brick_coords[1]
-        x2 = brick_coords[2]
-        y2 = brick_coords[3]
-
-        # we can then get a list of all objects in that area
-        results = canvas.find_overlapping(x1, y1, x2, y2)
-
-        # checks whether the ball hits the each brick
-        is_hit_this_brick = len(results)
-        if is_hit_this_brick > 1:
+        if is_hit_this_brick:
             color = canvas.itemcget(brick, "fill")
-            # if the ball hits the red bricks, red bricks become yellow
             if color == 'red':
+                canvas.itemconfig(brick, fill="purple")
+            elif color == 'purple':
                 canvas.itemconfig(brick, fill="yellow")
-                return True
-            # if the ball hits the yellow bricks, yellow bricks become blue
             elif color == 'yellow':
                 canvas.itemconfig(brick, fill="blue")
-                return True
-            # if the ball hits the blue bricks, blue bricks will be deleted from canvas and removed from the bricks list
             elif color == 'blue':
                 canvas.delete(brick)
                 bricks.remove(brick)
-                return True
+
+            return True
+
     return False
 
 
-def hit_left_wall(canvas, object):
+def ball_hits_left_wall(canvas, canvas_object):
     # returns the condition - ball hits left wall
-    return get_left_x(canvas, object) <= 0
+    return get_left_x(canvas, canvas_object) <= 0
 
 
-def hit_top_wall(canvas, object):
+def ball_hits_top_wall(canvas, canvas_object):
     # returns the condition - ball hits top wall
-    return get_top_y(canvas, object) <= 0
+    return get_top_y(canvas, canvas_object) <= 0
 
 
-def hit_right_wall(canvas, object):
+def ball_hits_right_wall(canvas, canvas_object):
     # returns the condition - ball hits right wall
-    return get_right_x(canvas, object) >= CANVAS_WIDTH
+    return get_right_x(canvas, canvas_object) >= CANVAS_WIDTH
 
 
-def hit_bottom_wall(canvas, object):
-    # returns the condition - ball hits bottom wall
-    return get_bottom_y(canvas, object) >= CANVAS_HEIGHT
-
-
-def get_left_x(canvas, object):
+def get_left_x(canvas, canvas_object):
     # returns the left x location of the object
-    return canvas.coords(object)[0]
+    return canvas.coords(canvas_object)[0]
 
 
-def get_top_y(canvas, object):
+def get_top_y(canvas, canvas_object):
     # returns the top y location of the object
-    return canvas.coords(object)[1]
+    return canvas.coords(canvas_object)[1]
 
 
-def get_right_x(canvas, object):
+def get_right_x(canvas, canvas_object):
     # returns the right x location of the object
-    return canvas.coords(object)[2]
+    return canvas.coords(canvas_object)[2]
 
 
-def get_bottom_y(canvas, object):
+def get_bottom_y(canvas, canvas_object):
     # returns the bottom y location of the object
-    return canvas.coords(object)[3]
+    return canvas.coords(canvas_object)[3]
 
 
 def make_canvas(width, height, title):
@@ -283,18 +348,14 @@ def make_canvas(width, height, title):
     top.minsize(width=width, height=height)
     top.title(title)
     canvas = tkinter.Canvas(top, width=width + 1, height=height + 1)
-    # canvas.bind("<Button-1>", mouse_clicked_callback)
     canvas.pack()
 
     return canvas
 
 
-def press_space():
+def space_pressed():
     # returns True if player press space
-    if keyboard.is_pressed('Space'):
-        return True
-    else:
-        return False
+    return keyboard.is_pressed('Space')
 
 
 if __name__ == '__main__':
